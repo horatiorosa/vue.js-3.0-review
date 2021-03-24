1b5d41ac5af55223a7fcf40a8b750cda41be9182
horatio <horatiorosa@aol.com>
2021-03-24 17:28:39 -0400

    L3 complete Attribute Binding

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
+const app = Vue.createApp( {
+  data( ) {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      alt: 'image of green socks',
+      url: 'http://daystolive.net/',
+      target: '_blank'
     }
+  }
 })
