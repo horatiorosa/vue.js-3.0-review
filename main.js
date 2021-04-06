5c39704f8a163a846878466e5458ec73669372f4
horatio <horatiorosa@aol.com>
2021-04-06 18:02:32 -0400

    L10 complete - Communicating Events

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
-})
+    methods: {
+      addToCart(variant) {
+        // this.cart += 1;
+        this.cart.push(variant.id);
+
+        console.log('cart', this.cart);
+      },
+
+      removeFromCart(variant, array) {
+        const index = this.cart.indexOf(variant.id);
+
+        if (index > -1) {
+          return this.cart = [
+            ...this.cart.slice(0, index),
+            ...this.cart.slice(index + 1),
+          ];
+          // console.log('...this.cart.slice(0, index)', ...this.cart.slice(0, index));
+          // console.log('...this.cart.slice(index + 1)', ...this.cart.slice(index + 1));
+        } else {
+          console.log(`You have no more of the ${product.color} socks in your cart.`);
+        }
+      }
+
+      // removeById(id) {
+      //   const index = this.cart.indexOf(id);
+      //   if (index > -1) {
+      //     this.cart.splice(index, 1);
+      //   }
+      // }
+    }
+});
+
+
